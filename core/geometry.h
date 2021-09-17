35f43469f257dbb1c742f85f0114b950cccb8349
hdgbdn <hdgbdn92@gmail.com>
2021-09-17 16:51:52 +0800

    finish Point3

--- core/geometry.h
+++ core/geometry.h
@@ -3,7 +3,7 @@
 
 #include <cassert>
 #include <iostream>
-#include <fmt/core.h>
+#include "pbrt.h"
 
 namespace pbrt
 {
@@ -23,6 +23,56 @@ namespace pbrt
 			assert(i >= 0 && i <= 1);
 			if (i == 0) return x;
 			return y;
+		}
+		Vector2<T> operator+(const Vector2<T>& rhs) const
+		{
+			return Vector2<T>(x + rhs.x, y + rhs.y);
+		}
+		Vector2<T>& operator+=(const Vector2<T>& rhs)
+		{
+			x += rhs.x; y += rhs.y;;
+			return *this;
+		}
+		Vector2<T> operator-(const Vector2<T>& rhs)
+		{
+			return Vector2<T>(x - rhs.x, y - rhs.y);
+		}
+		Vector2<T>& operator-=(const Vector2<T>& rhs)
+		{
+			x -= rhs.x; y -= rhs.y;
+			return *this;
+		}
+		Vector2<T> operator*(T s) const
+		{
+			return Vector2<T>(s * x, s * y);
+		}
+		Vector2<T>& operator*=(T s)
+		{
+			x *= s; y *= s;
+			return *this;
+		}
+		Vector2<T> operator/(T f) const
+		{
+			assert(f != 0);
+			float inv = static_cast<float>(1 / f);
+			return Vector2(x * inv, y * inv);
+		}
+		Vector2<T>& operator/=(T f)
+		{
+			assert(f != 0);
+			float inv = static_cast<float>(1 / f);
+			x *= inv; y *= inv;
+			return *this;
+		}
+		Vector2<T> operator-()
+		{
+			return Vector2<T>(-x, -y);
+		}
+		float LengthSquared() const { return x * x + y * y; }
+		float Length() const { return std::sqrt(LengthSquared()); }
+		bool HasNaNs() const
+		{
+			return std::isnan(x) || std::isnan(y);
 		}
 		T x, y;
 	};
@@ -117,8 +167,8 @@ namespace pbrt
 	template<typename T>
 	Vector3<T> Cross(const Vector3<T> &lhs, const Vector3<T> &rhs)
     {
-		double v1x = v1.x, v1y = v1.y, v1z = v1.z;
-		double v2x = v2.x, v2y = v2.y, v2z = v2.z;
+		double v1x = lhs.x, v1y = lhs.y, v1z = lhs.z;
+		double v2x = rhs.x, v2y = rhs.y, v2z = rhs.z;
 		return Vector3<T>(
 			(v1y * v2z) - (v1z * v2y),
 			(v1z * v2x) - (v1x * v2z),
@@ -211,7 +261,18 @@ namespace pbrt
 	{
 	public:
 		Point2() = default;
-		Point2(T xx, T yy) {};
+		Point2(T xx, T yy): x(xx), y(yy)
+		{
+			assert(!HasNaNs());
+		}
+		Point2(const Point3<T> &p): x(p.x), y(p.y)
+		{
+			assert(!HasNaNs());
+		}
+		bool HasNaNs() const
+		{
+			return std::isnan(x) || std::isnan(y);
+		}
 		T x, y;
 	};
 	template <typename T>
@@ -219,9 +280,165 @@ namespace pbrt
 	{
 	public:
 		Point3() = default;
-		Point3(T xx, T yy, T zz) {};
+		Point3(T xx, T yy, T zz):x(xx), y(yy), z(zz)
+		{
+			assert(!HasNaNs());
+		}
+
+		template<typename U>
+		explicit Point3(const Point3<U> &p)
+			:x(static_cast<T>(p.x)), y(static_cast<T>(p.y)), z(static_cast<T>(p.z))
+		{
+			assert(!HasNaNs());
+		}
+
+		template<typename U>
+		explicit operator Vector3<U>() const
+		{
+			return Vector3<U>(x, y, z);
+		}
+
+		T operator[](size_t i) const
+		{
+			assert(i <= 2);
+			if (i == 0) return x;
+			if (i == 1) return y;
+			return z;
+		}
+
+		T& operator[](size_t i)
+		{
+			assert(i <= 2);
+			if (i == 0) return x;
+			if (i == 1) return y;
+			return z;
+		}
+
+		Point3<T> operator+(const Vector3<T> &v) const
+		{
+			return Point3<T>(x + v.x, y + v.y, z + v.z);
+		}
+
+		Point3<T> &operator+=(const Vector3<T> &v)
+		{
+			x += v.x; y += v.y; z += v.z;
+			return *this;
+		}
+
+		Vector3<T> operator-(const Point3<T> &p) const
+		{
+			return Vector3<T>(x - p.x, y - p.y, z - p.z);
+		}
+
+		Point3<T> operator-(const Vector3<T> &v) const
+		{
+			return Point3<T>(x - v.x, y - v.y, z - v.z);
+		}
+
+		Point3<T>& operator-=(const Vector3<T>& v)
+		{
+			x -= v.x; y -= v.y; z -= v.z;
+			return *this;
+		}
+
+		Point3<T> operator+(const Point3<T> &rhs)
+		{
+			return Point3<T>(x + rhs.x, y + rhs.y, z + rhs.z);
+		}
+
+		template<typename U>
+		Point3<T> operator*(U s) const
+		{
+			return Point3<T>(s * x, s * y, s * z);
+		}
+
+		template<typename U>
+		Point3<T>& operator*=(U s)
+		{
+			x *= s; y *= s; z *= s;
+			return *this;
+		}
+
+		bool HasNaNs() const
+		{
+			return std::isnan(x) || std::isnan(y) || std::isnan(z);
+		}
 		T x, y, z;
 	};
+
+	template <typename T, typename U>
+	Point3<T> operator*(U f, const Point3<T>& p) {
+		return p * f;
+	}
+
+	template <typename T>
+	float Distance(const Point3<T> &p1, const Point3<T> &p2)
+	{
+		return (p1 - p2).Length();
+	}
+
+	template<typename T>
+	float DistanceSquared(const Point3<T> &p1, const Point3<T> &p2)
+	{
+		return (p1 - p2).LengthSquared();
+	}
+
+	template<typename T>
+	Point3<T> Lerp(float t, const Point3<T> &p0, const Point3<T> &p1)
+	{
+		return (1 - t) * p0 + t * p1;
+	}
+
+	template<typename T>
+	Point3<T> Min(const Point3<T> &p1, const Point3<T> &p2)
+	{
+		return Point3<T>(
+			std::min(p1.x, p2.x),
+			std::min(p1.y, p2.y),
+			std::min(p1.z, p2.z)
+			);
+	}
+
+	template<typename T>
+	Point3<T> Max(const Point3<T>& p1, const Point3<T>& p2)
+	{
+		return Point3<T>(
+			std::max(p1.x, p2.x),
+			std::max(p1.y, p2.y),
+			std::max(p1.z, p2.z)
+			);
+	}
+
+	template<typename T>
+	Point3<T> Floor(const Point3<T> &p)
+	{
+		return (std::floor(p.x), std::floor(p.y), std::floor(p.z));
+	}
+
+	template<typename T>
+	Point3<T> Ceil(const Point3<T>& p)
+	{
+		return (std::ceil(p.x), std::ceil(p.y), std::ceil(p.z));
+	}
+
+	template<typename T>
+	Point3<T> Abs(const Point3<T>& p)
+	{
+		return (std::abs(p.x), std::abs(p.y), std::abs(p.z));
+	}
+
+	template<typename T>
+	Point3<T> Permute(const Point3<T> &p, size_t x, size_t y, size_t z)
+	{
+		return Point3<T>(p[x], p[y], p[z]);
+	}
+
+	template<typename T>
+	std::ostream& operator<<(std::ostream& os, const Point3<T>& v)
+	{
+		os << fmt::format("[ {}, {}, {} ]", v.x, v.y, v.z);
+		return os;
+	}
 
 	typedef Point2<float> Point2f;
 	typedef Point2<int> Point2i;
@@ -242,25 +459,6 @@ namespace pbrt
 			return b.pMin != pMin || b.pMax != pMax;
 		}
 	};
-	template <typename T>
-	class Bounds3 {};
-
-	typedef Bounds2<float> Bounds2f;
-	typedef Bounds2<int> Bounds2i;
-	typedef Bounds3<float> Bounds3f;
-	typedef Bounds3<int> Bounds3i;
-
-	class Bounds2iIterator : public std::forward_iterator_tag
-	{
-	public:
-		Bounds2iIterator operator++();
-		Bounds2iIterator operator++(int);
-		bool operator==(const Bounds2iIterator &bi) const;
-		bool operator!= (const Bounds2iIterator &bi) const;
-		Point2i operator*() const;
-	};
-	inline Bounds2iIterator begin(const Bounds2i& b);
-	inline Bounds2iIterator end(const Bounds2i& e);
 
 	template <typename T>
 	class Normal2{};
@@ -273,12 +471,6 @@ namespace pbrt
 
 	typedef Normal3<float> Normal3f;
 
-	class Ray{};
-	class RayDifferential : public Ray
-	{
-	public:
-		void ScaleDifferentials(float s);
-	};
 
 	template <typename T>
 	inline T Dot(const Vector3<T>& v1, const Vector3<T>& v2) {
@@ -302,6 +494,32 @@ namespace pbrt
 		//DCHECK(!n1.HasNaNs() && !n2.HasNaNs());
 		return std::abs(n1.x * n2.x + n1.y * n2.y + n1.z * n2.z);
 	}
+
+	class Ray {};
+	class RayDifferential : public Ray
+	{
+	public:
+		void ScaleDifferentials(float s);
+	};
+	template <typename T>
+	class Bounds3 {};
+
+	typedef Bounds2<float> Bounds2f;
+	typedef Bounds2<int> Bounds2i;
+	typedef Bounds3<float> Bounds3f;
+	typedef Bounds3<int> Bounds3i;
+
+	class Bounds2iIterator : public std::forward_iterator_tag
+	{
+	public:
+		Bounds2iIterator operator++();
+		Bounds2iIterator operator++(int);
+		bool operator==(const Bounds2iIterator& bi) const;
+		bool operator!= (const Bounds2iIterator& bi) const;
+		Point2i operator*() const;
+	};
+	inline Bounds2iIterator begin(const Bounds2i& b);
+	inline Bounds2iIterator end(const Bounds2i& e);
 }
 
 #endif
--- test.cpp
+++ test.cpp
@@ -3,15 +3,10 @@
 using namespace pbrt;
 int main()
 {
-	Vector3f vec1(1.93234234f, 2.1f, 3.5f);
-	Vector3f vec2(1.93234234f, 2.1f, 3.5f);
-	std::cout << vec1 << std::endl;
-    std::cout << vec2 - vec1 << std::endl;
-    vec2 -= vec1;
-    std::cout << vec2 << std::endl;
-    vec2 += vec1;
-    std::cout << vec1 << std::endl;
-    vec2 *= 3.f;
-    std::cout << vec2 << std::endl;
-    std::cout << 3 * vec2 << std::endl;
+	Point3f p1(1.3f, 1.2f, 3.9f);
+	Point3f p2(1.2f, 9.3f, 5.8f);
+	Vector3f v1(2.4f, 3.6f, 9.3f);
+	auto p3 = p2 + v1;
+	auto p4 = Lerp(.4f, p1, p2);
+	std::cout << p4;
 }

8a0b5c1aa5a17ba88033304cffd9887466d8d3d6
hdgbdn <hdgbdn92@gmail.com>
2021-09-28 09:37:47 +0800

    finish disk

--- shapes/disk.cpp
+++ shapes/disk.cpp
@@ -24,8 +24,53 @@ namespace pbrt
 		if (tShapeHit <= 0 || tShapeHit >= ray.tMax) return false;
 		Point3f pHit = ray(tShapeHit);
 		float distSqrt = pHit.x * pHit.x + pHit.y * pHit.y;
-		if()
+		if (distSqrt > radius * radius || distSqrt < innerRadius * innerRadius)
+			return false;
+		float phi = std::atan2(pHit.y, pHit.x);
+		if (phi < 0) phi += 2 * Pi;
+		if (phi > phiMax) return false;
+		float u = phi / phiMax;
+		float rHit = std::sqrt(distSqrt);
+		float oneMinusV = ((rHit - innerRadius) / (radius - innerRadius));
+		float v = 1 - oneMinusV;
+		Vector3f dpdu(-phiMax * pHit.y, phiMax * pHit.x, 0);
+		Vector3f dpdv = Vector3f(pHit.x, pHit.y, 0.f)
+			* (innerRadius - radius) / rHit;
+		Normal3f dndu(0, 0, 0), dndv(0, 0, 0);
+
+		// Refine disk intersection point
+		pHit.z = height;
+
+		Vector3f pError(0, 0, 0);
+		*isect = (*ObjectToWorld)(SurfaceInteraction(pHit, pError, Point2f(u, v),
+			-ray.d, dpdu, dpdv, dndu, dndv,
+			ray.time, this));
+		*tHit = tShapeHit;
+		return true;
 	}
 
+	bool Disk::IntersectP(const Ray& r, bool testAlphaTexture) const
+	{
+		Vector3f oErr, dErr;
+		Ray ray = (*WorldToObject)(r, &oErr, &dErr);
+		EFloat ox(ray.o.x, oErr.x), oy(ray.o.y, oErr.y), oz(ray.o.z, oErr.z);
+		EFloat dx(ray.d.x, dErr.x), dy(ray.d.y, dErr.y), dz(ray.d.z, dErr.z);
+		if (ray.d.z == 0) return false;
+		float tShapeHit = (height - ray.o.z) / ray.d.z;
+		if (tShapeHit <= 0 || tShapeHit >= ray.tMax) return false;
+		Point3f pHit = ray(tShapeHit);
+		float distSqrt = pHit.x * pHit.x + pHit.y * pHit.y;
+		if (distSqrt > radius * radius || distSqrt < innerRadius * innerRadius)
+			return false;
+		float phi = std::atan2(pHit.y, pHit.x);
+		if (phi < 0) phi += 2 * Pi;
+		if (phi > phiMax) return false;
+		return true;
+	}
+
+	float Disk::Area()
+	{
+		return phiMax * 0.5 * (radius * radius - innerRadius * innerRadius);
+	}
 
 }

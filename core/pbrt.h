f389f181e7fe95ac366a5d9bbd612d7c2c85e45f
hdgbdn <hdgbdn92@gmail.com>
2021-12-04 16:01:42 +0800

    add constexpr

--- core/pbrt.h
+++ core/pbrt.h
@@ -15,7 +15,6 @@ namespace pbrt
 #ifdef _MSC_VER
 	static constexpr float MaxFloat = std::numeric_limits<float>::max();
 	static constexpr float Infinity = std::numeric_limits<float>::infinity();
-	static constexpr float Pi = 3.14159265358979323846f;
 #else
 	static PBRT_CONSTEXPR Float MaxFloat = std::numeric_limits<Float>::max();
 	static PBRT_CONSTEXPR Float Infinity = std::numeric_limits<Float>::infinity();
@@ -87,6 +86,17 @@ namespace pbrt
 
 	// samplers
 	class RNG;
+
+	// Global constants
+
+	static constexpr float ShadowEpsilon = 0.0001f;
+	static constexpr float Pi = 3.14159265358979323846;
+	static constexpr float InvPi = 0.31830988618379067154;
+	static constexpr float Inv2Pi = 0.15915494309189533577;
+	static constexpr float Inv4Pi = 0.07957747154594766788;
+	static constexpr float PiOver2 = 1.57079632679489661923;
+	static constexpr float PiOver4 = 0.78539816339744830961;
+	static constexpr float Sqrt2 = 1.41421356237309504880;
 
 	inline float Radians(float deg) { return (Pi / 180) * deg; }
 

4682c27d9b3832ee3bcb610e8f947d9c51aa0804
hdgbdn <hdgbdn92@gmail.com>
2021-12-11 14:26:02 +0800

    add solvelinearsystem

--- core/transformation.h
+++ core/transformation.h
@@ -73,6 +73,18 @@ namespace pbrt
     Matrix4x4 Inverse(const Matrix4x4& m);
     Matrix4x4 Transpose(const Matrix4x4& m);
 
+    bool SolveLinearSystem2x2(const float A[2][2],
+        const float B[2], float* x0, float* x1) {
+        float det = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+        if (std::abs(det) < 1e-10f)
+            return false;
+        *x0 = (A[1][1] * B[0] - A[0][1] * B[1]) / det;
+        *x1 = (A[0][0] * B[1] - A[1][0] * B[0]) / det;
+        if (std::isnan(*x0) || std::isnan(*x1))
+            return false;
+        return true;
+    }
+
 	class Transform
 	{
         friend Transform Inverse(const Transform& t);

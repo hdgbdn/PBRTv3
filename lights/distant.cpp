1e5fb32e25cd2ab8f264f76ffb923ba78970cf60
hdgbdn <hdgbdn92@gmail.com>
2021-12-18 11:55:55 +0800

    distance light

---
+++ lights/distant.cpp
@@ -0,0 +1,27 @@
+#include "distant.h"
+
+namespace pbrt
+{
+	DistantLight::DistantLight(const Transform& LightToWorld, const Spectrum& L, const Vector3f& wLight)
+		:Light((int)LightFlags::DeltaDirection, LightToWorld, MediumInterface()),
+	L(L), wLight(Normalize(LightToWorld(wLight)))
+	{
+	}
+	void DistantLight::Preprocess(const Scene& scene)
+	{
+		scene.Worldbound().BoundingSphere(&worldCenter, &worldRadius);
+	}
+	Spectrum DistantLight::Sample_Li(const Interaction& ref, const Point2f& u, Vector3f* wi, float* pdf, VisibilityTester* vis) const
+	{
+		*wi = wLight;
+		*pdf = 1;
+		Point3f pOutSide = ref.p + wLight * 2 * worldRadius;
+		*vis = VisibilityTester(ref, Interaction(pOutSide, ref.time,
+			mediumInterface));
+		return L;
+	}
+	Spectrum DistantLight::Power() const
+	{
+		return L * Pi * worldRadius * worldRadius;
+	}
+}
---
+++ lights/distant.h
@@ -0,0 +1,23 @@
+#ifndef PBRT_LIGHTS_DISTANT_H
+#define PBRT_LIGHTS_DISTANT_H
+
+#include "light.h"
+
+namespace pbrt
+{
+	class DistantLight : public Light
+	{
+	public:
+		DistantLight(const Transform& LightToWorld, const Spectrum& L, const Vector3f& wLight);
+		void Preprocess(const Scene& scene);
+		Spectrum Sample_Li(const Interaction& ref, const Point2f& u, Vector3f* wi, float* pdf, VisibilityTester* vis) const override;
+		Spectrum Power() const override;
+	private:
+		const Spectrum L;
+		const Vector3f wLight;
+		Point3f worldCenter;
+		float worldRadius;
+	};
+}
+
+#endif

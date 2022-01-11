65751e6bbffa7c588a2b8cf0a5acaa96b7857657
hdgbdn <hdgbdn92@gmail.com>
2022-01-11 11:58:40 +0800

    memory now is msvc only

--- core/memory.cpp
+++ core/memory.cpp
@@ -5,25 +5,12 @@ namespace pbrt
 {
 	void* AllocAligned(size_t size)
 	{
-#if defined(PBRT_IS_WINDOWS)
 		return _aligned_malloc(size, PBRT_L1_CACHE_LINE_SIZE);
-#elif defined(PBRT_IS_OPENBSD) || defined(PBRT_IS_OSX)
-		void* ptr;
-		if (posix_memalign(&ptr, PBRT_L1_CACHE_LINE_SIZE, size) != 0)
-			ptr = nullptr;
-		return ptr;
-#else
-		return memalign(PBRT_L1_CACHE_LINE_SIZE, size);
-#endif
 	}
 
 	void FreeAligned(void* ptr)
 	{
 		if (!ptr) return;
-#if defined(PBRT_HAVE__ALIGNED_MALLOC)
 		_aligned_free(ptr);
-#else
-		free(ptr);
-#endif
 	}
 }
--- core/memory.h
+++ core/memory.h
@@ -13,87 +13,95 @@ namespace pbrt
 		return (T*)AllocAligned(count * sizeof(T));
 	}
 	void FreeAligned(void*);
-    class MemoryArena{
-public:
-    // MemoryArena Public Methods
-    MemoryArena(size_t blockSize = 262144) : blockSize(blockSize) {}
-    ~MemoryArena() {
-        FreeAligned(currentBlock);
-        for (auto& block : usedBlocks) FreeAligned(block.second);
-        for (auto& block : availableBlocks) FreeAligned(block.second);
-    }
-    void* Alloc(size_t nBytes) {
-        // Round up _nBytes_ to minimum machine alignment
-#if __GNUC__ == 4 && __GNUC_MINOR__ < 9
-        // gcc bug: max_align_t wasn't in std:: until 4.9.0
-        const int align = alignof(::max_align_t);
-#elif !defined(PBRT_HAVE_ALIGNOF)
-        const int align = 16;
-#else
-        const int align = alignof(std::max_align_t);
-#endif
-#ifdef PBRT_HAVE_CONSTEXPR
-        static_assert(IsPowerOf2(align), "Minimum alignment not a power of two");
-#endif
-        nBytes = (nBytes + align - 1) & ~(align - 1);
-        if (currentBlockPos + nBytes > currentAllocSize) {
-            // Add current block to _usedBlocks_ list
-            if (currentBlock) {
-                usedBlocks.push_back(
-                    std::make_pair(currentAllocSize, currentBlock));
-                currentBlock = nullptr;
-                currentAllocSize = 0;
-            }
 
-            // Get new block of memory for _MemoryArena_
+	class MemoryArena
+	{
+	public:
+		// MemoryArena Public Methods
+		MemoryArena(size_t blockSize = 262144) : blockSize(blockSize)
+		{
+		}
 
-            // Try to get memory block from _availableBlocks_
-            for (auto iter = availableBlocks.begin();
-                 iter != availableBlocks.end(); ++iter) {
-                if (iter->first >= nBytes) {
-                    currentAllocSize = iter->first;
-                    currentBlock = iter->second;
-                    availableBlocks.erase(iter);
-                    break;
-                }
-            }
-            if (!currentBlock) {
-                currentAllocSize = std::max(nBytes, blockSize);
-                currentBlock = AllocAligned<uint8_t>(currentAllocSize);
-            }
-            currentBlockPos = 0;
-        }
-        void* ret = currentBlock + currentBlockPos;
-        currentBlockPos += nBytes;
-        return ret;
-    }
-    template <typename T>
-    T * Alloc(size_t n = 1, bool runConstructor = true) {
-        T* ret = (T*)Alloc(n * sizeof(T));
-        if (runConstructor)
-            for (size_t i = 0; i < n; ++i) new (&ret[i]) T();
-        return ret;
-    }
-    void Reset() {
-        currentBlockPos = 0;
-        availableBlocks.splice(availableBlocks.begin(), usedBlocks);
-    }
-    size_t TotalAllocated() const {
-        size_t total = currentAllocSize;
-        for (const auto& alloc : usedBlocks) total += alloc.first;
-        for (const auto& alloc : availableBlocks) total += alloc.first;
-        return total;
-    }
+		~MemoryArena()
+		{
+			FreeAligned(currentBlockStartPos);
+			for (auto& block : usedBlocks) FreeAligned(block.second);
+			for (auto& block : availableBlocks) FreeAligned(block.second);
+		}
 
-  private:
-    MemoryArena(const MemoryArena&) = delete;
-    MemoryArena& operator=(const MemoryArena&) = delete;
-    // MemoryArena Private Data
-    const size_t blockSize;
-    size_t currentBlockPos = 0, currentAllocSize = 0;
-    uint8_t* currentBlock = nullptr;
-    std::list<std::pair<size_t, uint8_t*>> usedBlocks, availableBlocks;
-    };
+		void* Alloc(size_t nBytes)
+		{
+			// Round up _nBytes_ to minimum machine alignment
+			nBytes = (nBytes + 16 - 1) & ~(16 - 1);
+			if (currentBlockPos + nBytes > currentAllocSize)
+			{
+				// Add current block to _usedBlocks_ list
+				if (currentBlockStartPos)
+				{
+					usedBlocks.push_back(
+						std::make_pair(currentAllocSize, currentBlockStartPos));
+					currentBlockStartPos = nullptr;
+					currentAllocSize = 0;
+				}
+
+				// Get new block of memory for _MemoryArena_
+
+				// Try to get memory block from _availableBlocks_
+				for (auto iter = availableBlocks.begin();
+				     iter != availableBlocks.end(); ++iter)
+				{
+					if (iter->first >= nBytes)
+					{
+						currentAllocSize = iter->first;
+						currentBlockStartPos = iter->second;
+						availableBlocks.erase(iter);
+						break;
+					}
+				}
+				if (!currentBlockStartPos)
+				{
+					currentAllocSize = std::max(nBytes, blockSize);
+					currentBlockStartPos = AllocAligned<uint8_t>(currentAllocSize);
+				}
+				currentBlockPos = 0;
+			}
+			void* ret = currentBlockStartPos + currentBlockPos;
+			currentBlockPos += nBytes;
+			return ret;
+		}
+
+		template <typename T>
+		T* Alloc(size_t n = 1, bool runConstructor = true)
+		{
+			T* ret = (T*)Alloc(n * sizeof(T));
+			if (runConstructor)
+				for (size_t i = 0; i < n; ++i) new(&ret[i]) T();
+			return ret;
+		}
+
+		void Reset()
+		{
+			currentBlockPos = 0;
+			availableBlocks.splice(availableBlocks.begin(), usedBlocks);
+		}
+
+		size_t TotalAllocated() const
+		{
+			size_t total = currentAllocSize;
+			for (const auto& alloc : usedBlocks) total += alloc.first;
+			for (const auto& alloc : availableBlocks) total += alloc.first;
+			return total;
+		}
+
+	private:
+		MemoryArena(const MemoryArena&) = delete;
+		MemoryArena& operator=(const MemoryArena&) = delete;
+		// MemoryArena Private Data
+		const size_t blockSize;
+		size_t currentBlockPos = 0, currentAllocSize = 0;
+		uint8_t* currentBlockStartPos = nullptr;
+		std::list<std::pair<size_t, uint8_t*>> usedBlocks, availableBlocks;
+	};
 
     template <typename T, int logBlockSize>
     class BlockedArray {
@@ -108,7 +116,7 @@ public:
                 for (int v = 0; v < vRes; ++v)
                     for (int u = 0; u < uRes; ++u) (*this)(u, v) = d[v * uRes + u];
         }
-        constexpr  int BlockSize() const { return 1 << logBlockSize; }
+        constexpr int BlockSize() const { return 1 << logBlockSize; }
         int RoundUp(int x) const {
             return (x + BlockSize() - 1) & ~(BlockSize() - 1);
         }
@@ -138,7 +146,11 @@ public:
             for (int v = 0; v < vRes; ++v)
                 for (int u = 0; u < uRes; ++u) *a++ = (*this)(u, v);
         }
-
+		void Print()
+        {
+			for (int i = 0; i < RoundUp(uRes) * RoundUp(vRes); ++i)
+				fmt::print("{} ", data[i]);
+        }
     private:
         // BlockedArray Private Data
         T* data;

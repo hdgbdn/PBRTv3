baf84e7cc04e9c90384e1cebb766a547e6a4d25e
hdgbdn <hdgbdn92@gmail.com>
2022-01-22 10:40:57 +0800

    change error interface

--- core/error.h
+++ core/error.h
@@ -1,11 +1,30 @@
 #ifndef PBRT_CORE_ERROR_H
 #define PBRT_CORE_ERROR_H
+#include "fmt/core.h"
 
 namespace pbrt
 {
-	void Info(const char* format, ...);
-	void Warning(const char* format, ...);
-	void Error(const char* format, ...);
+	template <typename... T>
+	void Info(const char* format, T&&... args)
+	{
+		fmt::print("Info: ");
+		//fmt::print((std::string)format, &args...);
+	}
+
+	template <typename... T>
+	void Warning(const char* format, T&&... args)
+	{
+		fmt::print("Warning: ");
+		//fmt::print((std::string)format, &args...);
+	}
+	
+	template <typename... T>
+	void Error(const char* format, T&&... args)
+	{
+		fmt::print("Error: ");
+		//fmt::print((std::string)format, &args...);
+	}
+
 }
 
 #endif // !PBRT_CORE_ERROR_H

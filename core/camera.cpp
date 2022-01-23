66f584c35cff54e44279decfbd17ddc0d21bd0a2
hdgbdn <hdgbdn92@gmail.com>
2022-01-23 14:31:28 +0800

    camera transform error fixed

--- core/camera.cpp
+++ core/camera.cpp
@@ -35,11 +35,12 @@ namespace pbrt
 		: Camera(CameraToWorld, shutterOpen, shutterClose, film, medium),
 		  CameraToScreen(CameraToScreen), lensRadius(lensr), focalDistance(focald)
 	{
-		ScreenToRaster = Scale(film->fullResolution.x,
-			film->fullResolution.y, 1) *
+		ScreenToRaster = 
+			Scale(film->fullResolution.x, film->fullResolution.y, 1) *
 			Scale(1 / (screenWindow.pMax.x - screenWindow.pMin.x),
 				1 / (screenWindow.pMin.y - screenWindow.pMax.y), 1) *
 			Translate(Vector3f(-screenWindow.pMin.x, -screenWindow.pMax.y, 0));
+		RasterToScreen = Inverse(ScreenToRaster);
 		RasterToCamera = Inverse(CameraToScreen) * RasterToScreen;
 	}
 
